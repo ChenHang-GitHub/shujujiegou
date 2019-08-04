e538b10b0d0980f4b3b32e0d2c45a76d360ed1a2
ChenHang-GitHub <704635857@qq.com>
2019-08-05 01:35:34 +0800

    shuju

--- src/shujujiegou/Git.java
+++ src/shujujiegou/Git.java
@@ -3,6 +3,6 @@ package shujujiegou;
 public class Git {
     public static void main(String[] args) {
 
-        System.out.println("Branch1     1change");
+        System.out.println("Branch1 ");
     }
 }
